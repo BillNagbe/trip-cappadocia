02ed5dd79038395423efa4a0d213c9e0311e768e
Bill Nagbe <BillyNagbe@gmail.com>
2025-01-25 13:18:32 -0600

    cappadocia starter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+import Nav from "./components/Header/Nav/Nav";
+import Main from "./components/Main/Main";
+import Aside from "./components/Aside/Aside";
+
+
+function App() {
+
+  return (
+    <div>
+      <Header name="Cappidocia" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const headerContainerStyle = {
+    width: "50%",
+    height: "fit-content",
+    padding: "2rem 4rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px dotted red",
+    margin: "5vh auto",
+}
+
+const titleStyle = {
+    color: "hsl(360, 90%, 56%)",
+    fontStyle: "italic",
+    letterSpacing: "0.25rem"
+}
+
+function Header(props) {
+
+    return (
+        <div 
+        style={headerContainerStyle}>
+        <h1 style={titleStyle}>Welcome to {props.name}</h1>
+        </div>
+    )
+};
+
+
+
+
+
+export default Header;
